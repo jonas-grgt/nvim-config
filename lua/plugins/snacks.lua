44532ae817527c63717008c33f579f189bac2599
JonasG <jonas.grgt@gmail.com>
2026-02-06 09:43:19 +0100

    fix(do not open explorer by default):

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,5 +2,15 @@ return {
   "snacks.nvim",
   opts = {
     scroll = { enabled = false },
+    picker = {
+      hidden = false,
+      ignored = true,
+      sources = {
+        explorer = {
+          hidden = false,
+          ignored = true,
+        },
+      },
+    },
   },
 }
